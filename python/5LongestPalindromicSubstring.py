a9043e8e80089bd6d907f68f26278e69ec01a10a
Mike Wiles <mike.lee.wiles@gmail.com>
2022-11-26 18:08:10 -0500

    Update 5LongestPalindromicSubstring.py

--- python/5LongestPalindromicSubstring.py
+++ python/5LongestPalindromicSubstring.py
@@ -17,31 +17,24 @@
 
 class Solution:
     def longestPalindrome(self, s: str):
-        index = 0
-        longest = 0
-        templongest = 0
-        starter = 0
-        tempstr = ""
-        while starter < len(s):
-            while index < len(s):
-                letter = s[index]
-                if tempstr and tempstr.find(letter) > -1:
-                    if templongest > longest:
-                        longest = templongest
-                    templongest = 0
-                    tempstr = letter
-                else:
-                    tempstr =  tempstr + letter
-                    templongest = len(tempstr)
-                index += 1   
-                if templongest > longest:
-                        longest = templongest
-            starter += 1
-            index = starter
-            tempstr = ""
-        return longest
+        frontchr = 0 #first character in the string
+        endchr = 0   #last character in the string
+        currentstr = ""
+        longeststr = ""
 
+        while frontchr < len(s): #keep front character from running off end of string
+            while endchr <= len(s): #keep end character from running off end of string
+                currentstr = s[frontchr:endchr]
+                if len(currentstr) > len(longeststr): #only check if length is longer
+                    if currentstr == currentstr[::-1]: #check to see if its a palindrone
+                        longeststr = currentstr
+                endchr += 1
+
+            frontchr += 1 #move the front character up a letter and rescan the string
+            endchr = frontchr #reset the end character to the front
+        return longeststr #return the longest string found
 
 sol = Solution()
 print(sol.longestPalindrome("babad"))
 print(sol.longestPalindrome("cbbd"))
+print(sol.longestPalindrome("gphyvqruxjmwhonjjrgumxjhfyupajxbjgthzdvrdqmdouuukeaxhjumkmmhdglqrrohydrmbvtuwstgkobyzjjtdtjroqpyusfsbjlusekghtfbdctvgmqzeybnwzlhdnhwzptgkzmujfldoiejmvxnorvbiubfflygrkedyirienybosqzrkbpcfidvkkafftgzwrcitqizelhfsruwmtrgaocjcyxdkovtdennrkmxwpdsxpxuarhgusizmwakrmhdwcgvfljhzcskclgrvvbrkesojyhofwqiwhiupujmkcvlywjtmbncurxxmpdskupyvvweuhbsnanzfioirecfxvmgcpwrpmbhmkdtckhvbxnsbcifhqwjjczfokovpqyjmbywtpaqcfjowxnmtirdsfeujyogbzjnjcmqyzciwjqxxgrxblvqbutqittroqadqlsdzihngpfpjovbkpeveidjpfjktavvwurqrgqdomiibfgqxwybcyovysydxyyymmiuwovnevzsjisdwgkcbsookbarezbhnwyqthcvzyodbcwjptvigcphawzxouixhbpezzirbhvomqhxkfdbokblqmrhhioyqubpyqhjrnwhjxsrodtblqxkhezubprqftrqcyrzwywqrgockioqdmzuqjkpmsyohtlcnesbgzqhkalwixfcgyeqdzhnnlzawrdgskurcxfbekbspupbduxqxjeczpmdvssikbivjhinaopbabrmvscthvoqqbkgekcgyrelxkwoawpbrcbszelnxlyikbulgmlwyffurimlfxurjsbzgddxbgqpcdsuutfiivjbyqzhprdqhahpgenjkbiukurvdwapuewrbehczrtswubthodv"))
